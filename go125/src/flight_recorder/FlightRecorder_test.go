aae9d286e238d2e57f246f9a377581446f66bc2f
crazytaxi <crazytaxi.git@outlook.com>
2025-09-23 10:53:10 +1000

    fix: synctest cause FlightRecorder time not accurate

--- go125/src/flight_recorder/FlightRecorder_test.go
+++ go125/src/flight_recorder/FlightRecorder_test.go
@@ -7,17 +7,24 @@ import (
 	"net/http"
 	"os"
 	"runtime/trace"
+	"strconv"
 	"sync"
 	"testing"
-	"testing/synctest"
 	"time"
 )
+
+func fibonacci(n int) int {
+	if n <= 1 {
+		return n
+	}
+	return fibonacci(n-1) + fibonacci(n-2)
+}
 
 func TestFlightRecorder(t *testing.T) {
 	// 新建记录
 	cfg := trace.FlightRecorderConfig{
 		MinAge:   5 * time.Second,
-		MaxBytes: 5 << 20, // 5MB
+		MaxBytes: 5 << 20, // 5MB, MaxBytes is the priority
 	}
 
 	fr := trace.NewFlightRecorder(cfg)
@@ -32,41 +39,37 @@ func TestFlightRecorder(t *testing.T) {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		// 使用新的 synctest 避免 wait 时间
-		synctest.Test(t, func(t *testing.T) {
-			if !fr.Enabled() {
-				t.Error("FlightRecorder is not Enabled")
-				return
-			}
+		if !fr.Enabled() {
+			t.Error("FlightRecorder is not Enabled")
+			return
+		}
 
-			start := time.Now()
+		start := time.Now()
 
-			// dowork()
-			time.Sleep(1 * time.Second)
-			synctest.Wait()
-			t.Log(r.RemoteAddr)
-			w.Write([]byte("hello world"))
+		// do heavy work()
+		fib := fibonacci(40)
+		t.Log(r.RemoteAddr)
+		w.Write([]byte("fibonacci: " + strconv.Itoa(fib)))
 
-			// trigger
-			if time.Since(start) > 300*time.Millisecond {
-				// 记录一次数据
-				once.Do(func() {
-					// Grab the snapshot.
-					var b bytes.Buffer
-					_, err := fr.WriteTo(&b)
-					if err != nil {
-						t.Error(err)
-						return
-					}
-					// Write it to a file.
-					// 使用 go tool trace ... 查看数据
-					if err := os.WriteFile("trace.out", b.Bytes(), 0o755); err != nil {
-						log.Print(err)
-						return
-					}
-				})
-			}
-
-		})
+		// trigger
+		if time.Since(start) > 300*time.Millisecond {
+			// 记录一次数据
+			once.Do(func() {
+				// Grab the snapshot.
+				var b bytes.Buffer
+				_, err := fr.WriteTo(&b)
+				if err != nil {
+					t.Error(err)
+					return
+				}
+				// Write it to a file.
+				// 使用 go tool trace ... 查看数据
+				if err := os.WriteFile("trace.out", b.Bytes(), 0o755); err != nil {
+					log.Print(err)
+					return
+				}
+			})
+		}
 	})
 
 	// 开启服务
