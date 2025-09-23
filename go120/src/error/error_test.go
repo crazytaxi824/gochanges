0e5c1ed57a97aaf5a75988dae19b4a16ae46c4d2
crazytaxi <crazytaxi.git@outlook.com>
2025-09-23 13:00:50 +1000

    update(go 1.20): error test

--- go120/src/error/error_test.go
+++ go120/src/error/error_test.go
@@ -38,7 +38,8 @@ func TestErrorAs(t *testing.T) {
 
 		var perr *fs.PathError
 
-		// 将 error 断言成 PathError, 便于使用 PathError.Path 属性.
+		// NOTE: 将 error 断言成 PathError 类型, 同时传入 perr (指针)中
+		// 便于使用 PathError.Path 属性.
 		if errors.As(err, &perr) {
 			t.Log("Failed at path:", perr.Path)
 		} else {
@@ -52,6 +53,7 @@ var (
 	ErrAuth = errors.New("auth error")
 )
 
+// Join 是将多个 error 添加到一起
 func TestErrorJoin(t *testing.T) {
 	// combine multiple errors
 	err := errors.Join(ErrDB, ErrAuth)
@@ -65,5 +67,34 @@ func TestErrorJoin(t *testing.T) {
 	}
 	if errors.Is(err, ErrAuth) {
 		t.Log("Contains ErrAuth")
+	}
+}
+
+type ErrA struct{}
+
+type ErrB struct{}
+
+func (*ErrA) Error() string {
+	return "ErrA"
+}
+
+func (*ErrB) Error() string {
+	return "ErrB"
+}
+
+func TestErrorJoin2(t *testing.T) {
+	err := errors.Join(&ErrA{}, &ErrB{})
+
+	t.Log("Joined error:", err)
+
+	// check error Type
+	var myErrA *ErrA
+	if errors.As(err, &myErrA) {
+		t.Log("As ErrA:", myErrA.Error())
+	}
+
+	var myErrB *ErrB
+	if errors.As(err, &myErrB) {
+		t.Log("As ErrB:", myErrB.Error())
 	}
 }
